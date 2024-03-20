3f1a61db05535bccb01f1f2bd235f4a40e8bbd64
JadirMiramontes <jadirescuela23@gmail.com>
2024-03-20 09:54:32 -0600

    Carpeta3_Video4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,11 @@ import { CounterComponent } from './counter/counter.component';
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[RouterOutlet],
+  imports:[RouterOutlet,CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
-  contador: number = 0;
-
-  add(){
-    this.contador++;
-  }
-
-  dis(){
-    this.contador--;
-  }
+ 
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -2,8 +2,17 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:"app-contador",
+    standalone:true,
     templateUrl:"./counter.component.html"
 })
 export class CounterComponent{
+    contador: number = 0;
 
+    add(){
+      this.contador++;
+    }
+  
+    dis(){
+      this.contador--;
+    }
 }
